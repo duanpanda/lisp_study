0c75ad7cba80766ae43eb8b9a2b94697cfac3d0c
Ryan Duan <duanpanda@gmail.com>
2018-08-04 00:26:55 -0400

    reformat code

--- landoflisp/dice-of-doom-v4.lisp
+++ landoflisp/dice-of-doom-v4.lisp
@@ -11,26 +11,30 @@
 	     (car (aref board pos)))
 	   (dice (pos)
              (cadr (aref board pos))))
-    (lazy-mapcan (lambda (src)
-                   (if (eq (player src) cur-player)
-                      (lazy-mapcan
-                        (lambda (dst)
-                          (if (and (not (eq (player dst) cur-player))
-                                   (> (dice src) 1))
-                              (make-lazy (list (list (list src dst)
-      (game-tree (board-attack board cur-player src dst (dice src))
-                 cur-player
-                 (+ spare-dice (dice dst))
-                 nil)
-      (game-tree (board-attack-fail board cur-player src dst (dice src))
-                 cur-player
-                 (+ spare-dice (dice dst))
-                 nil))))
-                                       (lazy-nil)))
-                                   (make-lazy (neighbors src)))
-                      (lazy-nil)))
-                 (make-lazy (loop for n below *board-hexnum*
-                           collect n)))))
+    (lazy-mapcan
+     (lambda (src)
+       (if (eq (player src) cur-player)
+	   (lazy-mapcan
+	    (lambda (dst)
+	      (if (and (not (eq (player dst) cur-player))
+		       (> (dice src) 1))
+		  (make-lazy
+		   (list
+		    (list
+		     (list src dst)
+		     (game-tree (board-attack board cur-player src dst (dice src))
+				cur-player
+				(+ spare-dice (dice dst))
+				nil)
+		     (game-tree (board-attack-fail board cur-player src dst (dice src))
+				cur-player
+				(+ spare-dice (dice dst))
+				nil))))
+		  (lazy-nil)))
+	    (make-lazy (neighbors src)))
+	   (lazy-nil)))
+     (make-lazy (loop for n below *board-hexnum*
+		   collect n)))))
 
 (defun board-attack-fail (board player src dst dice)
   (board-array (loop for pos from 0
